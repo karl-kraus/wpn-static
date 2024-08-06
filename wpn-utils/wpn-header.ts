04256a5845e6d209cb44b86186cbf200be284e14
Barbara Krautgartner <barbara.krautgartner@oeaw.ac.at>
2024-08-06 08:13:18 +0000

    feat: set style values based on dynamic header footer heights

--- wpn-utils/wpn-header.ts
+++ wpn-utils/wpn-header.ts
@@ -1,26 +1,28 @@
 class WPNHeader extends HTMLElement {
+	resizeObserver = new ResizeObserver((entries) => {
+		for (const entry of entries) {
+			const mainElement = document.querySelector("main");
+			const wpnDetailView = document.querySelector("wpn-detail-view");
+			const footer = document.querySelector("footer");
+			if (mainElement) {
+				mainElement.style.marginTop = `${String(entry.target.clientHeight)}px`;
+			}
+			if (wpnDetailView) {
+				(wpnDetailView as HTMLElement).style.top = `${String(entry.target.clientHeight)}px`;
+				if (footer) {
+					(wpnDetailView as HTMLElement).style.maxHeight =
+						`calc(100% - ${String(entry.target.clientHeight + footer.clientHeight)}px`;
+				}
+			}
+		}
+	});
 
-  resizeObserver = new ResizeObserver((entries) => {
-    for (const entry of entries) {
-        const mainElement = document.querySelector("main");
-        if (mainElement) {
-          mainElement.style.marginTop = `${String(entry.target.clientHeight)}px`;
-        }
-    }
-  })
-  
-  
-  
-  resizeHandler = () =>{ 
-    document.body.style.marginTop = `${String(this.clientHeight)})px`;  
-  }
-	
 	connectedCallback() {
-    this.resizeObserver.observe(this);
-  }
-  disconntedCallback() {
-    this.resizeObserver.unobserve(this);
-  }
+		this.resizeObserver.observe(this);
+	}
+	disconntedCallback() {
+		this.resizeObserver.unobserve(this);
+	}
 }
 
 customElements.define("wpn-header", WPNHeader);
