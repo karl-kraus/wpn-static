2927763ac3d3bc41f8ae59b9c8118138b1abc250
Barbara Krautgartner <barbara.krautgartner@oeaw.ac.at>
2024-08-06 08:21:46 +0000

    chore: publish latest build

---
+++ html/js/wpn_utils/init-mirador.BnzTCz68.js
@@ -0,0 +1,63 @@
+var _a;
+const manifestId = (_a = document.getElementById("mirador")) == null ? void 0 : _a.getAttribute("data-manifest");
+if (manifestId) {
+  const pathToManifest = `iiif_manifests/${manifestId}/manifest.json`;
+  const manifests = {
+    pathToManifest: {
+      "provider": "Karl Kraus 1933"
+    }
+  };
+  Mirador.viewer({
+    "id": "mirador",
+    "manifests": manifests,
+    "themes": {
+      "light": {
+        "palette": {
+          "type": "light",
+          "primary": {
+            "main": "#A21A17"
+          }
+        }
+      }
+    },
+    "language": "de",
+    "displayAllAnnotations": false,
+    "window": {
+      "allowClose": false,
+      "allowFullscreen": false,
+      "allowMaximize": false,
+      "allowTopMenuButton": true,
+      "hideWindowTitle": false,
+      "defaultSideBarPanel": "annotations",
+      "sideBarOpenByDefault": true,
+      "panels": {
+        "info": false,
+        "attribution": false,
+        "canvas": false,
+        "annotations": true,
+        "search": true
+      }
+    },
+    "workspaceControlPanel": {
+      "enabled": false
+      // Configure if the control panel should be rendered.  Useful if you want to lock the viewer down to only the configured manifests
+    },
+    "galleryView": {
+      height: null,
+      width: 50
+    },
+    "thumbnailNavigation": {
+      defaultPosition: "off",
+      // Which position for the thumbnail navigation to be be displayed. Other possible values are "far-bottom" or "far-right"
+      height: 200
+      // height of entire ThumbnailNavigation area when position is "far-bottom"
+    },
+    "windows": [
+      {
+        "loadedManifest": pathToManifest,
+        "canvasIndex": 0,
+        "thumbnailNavigationPosition": "off"
+      }
+    ]
+  });
+}
---
+++ html/js/wpn_utils/wpn-reg-entry.Wv7ETHzR.js
@@ -0,0 +1,22 @@
+var __defProp = Object.defineProperty;
+var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
+var __publicField = (obj, key, value) => __defNormalProp(obj, typeof key !== "symbol" ? key + "" : key, value);
+class WPNRegEntry extends HTMLElement {
+  constructor() {
+    super(...arguments);
+    __publicField(this, "clickHandler", () => {
+      this.notifyDetailView();
+    });
+    __publicField(this, "notifyDetailView", () => {
+      console.log(this.getAttribute("id"));
+      this.dispatchEvent(new CustomEvent("updateDetailView", { bubbles: true, detail: { id: this.getAttribute("id") } }));
+    });
+  }
+  connectedCallback() {
+    this.addEventListener("click", this.clickHandler);
+  }
+  disconntedCallback() {
+    this.removeEventListener("click", this.clickHandler);
+  }
+}
+customElements.define("wpn-reg-entry", WPNRegEntry);
