bfd9bb687e241133bdb9ffdc5129f449e20a6e58
Barbara Krautgartner <barbara.krautgartner@oeaw.ac.at>
2024-08-06 08:12:16 +0000

    feat: adapt scroll behavior and offset

--- wpn-utils/wpn-detail-view.ts
+++ wpn-utils/wpn-detail-view.ts
@@ -7,6 +7,11 @@ class WPNDetailView extends HTMLElement {
       const detailElement = this.querySelector(`[id^='details_${entityId}']`);
       this.querySelector("[id^='details_']:not(.d-none)")?.classList.add('d-none');
       detailElement?.classList.remove("d-none");
+      const anchorElement = document.getElementById(entityId);
+      if (anchorElement) {
+        anchorElement.style.scrollMarginTop = `${String(document.getElementsByTagName("wpn-header")[0].clientHeight)}px`
+        anchorElement.scrollIntoView()
+      }
       const popoverTriggerElement = detailElement?.querySelector("[data-bs-toggle='popover']")
       if (popoverTriggerElement) {
         console.log(popoverTriggerElement)
