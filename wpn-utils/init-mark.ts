649e136ad8052504287332f68e1c0042e39985ec
Barbara Krautgartner <barbara.krautgartner@oeaw.ac.at>
2024-09-09 14:56:06 +0000

    feat: introduce occurence param and jump to occurence

--- wpn-utils/init-mark.ts
+++ wpn-utils/init-mark.ts
@@ -1,6 +1,20 @@
-const markInstance = new Mark(document.querySelector("#textcontent"));
+const contentElement = document.querySelector("#textcontent");
+const headerElm = document.querySelector("wpn-header");
+const markInstance = new Mark(contentElement);
 const url = new URL(window.location.href);
 const urlParam = new URLSearchParams(url.search);
-if (urlParam.get("mark")) {
-	markInstance.mark(urlParam.get("mark"));
-}
+const markValue = urlParam.get("mark");
+const occurence = urlParam.get("occurence");
+if (markValue) {
+  markInstance.mark(markValue,{
+    done: () => {
+      const marks = contentElement.querySelectorAll("mark");
+      let matchNr = 0;
+      if (occurence && (/\d+/g).test(occurence)) {
+          matchNr = Number(occurence) - 1;
+        } 
+        const position = marks[matchNr].offsetTop - headerElm.clientHeight;
+        window.scrollTo(0, position);
+      }
+    })
+  };
