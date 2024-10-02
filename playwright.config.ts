ed204f47c5b705aadebc4a767d79125399a0eb62
Barbara Krautgartner <barbara.krautgartner@oeaw.ac.at>
2024-10-02 12:45:28 +0000

    ci: use html reporter, no retries

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,12 +17,11 @@ export default defineConfig({
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: isCI,
-  /* Retry on CI only */
-  retries: isCI ? 2 : 0,
+  retries: 0,
   /* Opt out of parallel tests on CI. */
   workers: isCI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: isCI ? "github" : [['html',{host:'0.0.0.0'}]],
+  reporter: isCI ? "html" : [['html',{host:'0.0.0.0'}]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
