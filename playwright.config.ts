ccc662bc37e5effe4958fb63840e534951cb087c
Barbara Krautgartner <barbara.krautgartner@oeaw.ac.at>
2024-10-02 08:09:00 +0000

    chore: start/reuse server for tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -77,9 +77,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'python3 -m http.server',
+    url: 'http://127.0.0.1:8000',
+    reuseExistingServer: !isCI,
+  },
 });
