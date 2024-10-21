3b0bd86a81f8a3a5bfa2a1d75c9b8ebd5ec18510
Barbara Krautgartner <barbara.krautgartner@oeaw.ac.at>
2024-10-21 09:41:54 +0000

    test: add helpers for fetching testdata and testing list items

---
+++ e2e/utils/helpers.ts
@@ -0,0 +1,29 @@
+import type { Page } from "@playwright/test";
+
+import type { TestDataObjectIntertext,TestDataObjectPerson } from "../src/interfaces.js";
+
+
+export async function fetchIntertextData(fileName: string) {
+  const data_module = await import(`../data/${fileName}`, {with: {type: "json"}}) as {default: Array<TestDataObjectIntertext>};
+  const TestData: Array<TestDataObjectIntertext> = data_module.default;
+  return TestData;
+};
+
+export async function fetchPersonData(fileName: string) {
+  const data_module = await import(`../data/${fileName}`, {with: {type: "json"}}) as {default: Array<TestDataObjectPerson>};
+  const TestData: Array<TestDataObjectPerson> = data_module.default;
+  return TestData;
+};
+
+export async function getListItems(page: Page, test_id: string, selectors: Array<string>) {
+  const listItemsLocator = page.getByTestId(test_id).locator('li');
+  const listItems = await listItemsLocator.evaluateAll((listItems, selectors) => {
+    return Array.from(listItems).map((item) => {
+      return {
+        label: item.querySelector(selectors[0])?.textContent,
+        text: item.querySelector(selectors[1])?.textContent,
+      };
+    });
+  }, selectors);
+  return listItems;
+}
