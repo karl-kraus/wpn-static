2fe057679024a08fe7a3b72cb967a3a6519ea982
Barbara Krautgartner <barbara.krautgartner@oeaw.ac.at>
2024-10-28 07:42:26 +0000

    feat: add/adjust helper(s) regarding comment tests

--- e2e/utils/helpers.ts
+++ e2e/utils/helpers.ts
@@ -1,6 +1,6 @@
 import type { Page } from "@playwright/test";
 
-import type { TestDataObjectIntertext,TestDataObjectPerson } from "../src/interfaces.js";
+import type { TestDataObjectComment,TestDataObjectIntertext,TestDataObjectPerson } from "../src/interfaces.js";
 
 
 export async function fetchIntertextData(fileName: string) {
@@ -15,14 +15,26 @@ export async function fetchPersonData(fileName: string) {
   return TestData;
 };
 
+export async function fetchCommentData(fileName: string) {
+  const data_module = await import(`../test-data/${fileName}`, {with: {type: "json"}}) as {default: Array<TestDataObjectComment>};
+  const TestData: Array<TestDataObjectComment> = data_module.default;
+  return TestData;
+};
+
 export async function getListItems(page: Page, test_id: string, selectors: Array<string>) {
   const listItemsLocator = page.getByTestId(test_id).locator('li');
   const listItems = await listItemsLocator.evaluateAll((listItems, selectors) => {
     return Array.from(listItems).map((item) => {
-      return {
-        label: item.querySelector(selectors[0])?.textContent,
-        text: item.querySelector(selectors[1])?.textContent,
-      };
+      const listItem: Record<string, string> = {};
+      
+      if (selectors.length > 1) {
+        listItem.label = item.querySelector(selectors[0])?.textContent?? "";
+        listItem.text = item.querySelector(selectors[1])?.textContent?? "";
+      } else {
+        listItem.text = item.querySelector(selectors[0])?.textContent?? "";
+        listItem.href = item.querySelector(selectors[0])?.getAttribute("href")?? "";
+      }
+      return listItem;
     });
   }, selectors);
   return listItems;
