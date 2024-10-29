e98509d68ab0665001c16dfe8a0be15e1d4c2d7b
Barbara Krautgartner <barbara.krautgartner@oeaw.ac.at>
2024-10-29 08:58:23 +0000

    feat: use generic function

--- e2e/utils/helpers.ts
+++ e2e/utils/helpers.ts
@@ -1,38 +1,29 @@
 import type { Page } from "@playwright/test";
 
-import type { TestDataObjectComment,TestDataObjectIntertext,TestDataObjectPerson } from "../src/interfaces.js";
-
-
-export async function fetchIntertextData(fileName: string) {
-  const data_module = await import(`../test-data/${fileName}`, {with: {type: "json"}}) as {default: Array<TestDataObjectIntertext>};
-  const TestData: Array<TestDataObjectIntertext> = data_module.default;
+export async function fetchTestData<T>(fileName: string): Promise<Array<T>> {
+  let TestData: Array<T>;
+  try {
+    const data_module = await import(`../test-data/${fileName}`, { with: { type: "json" } }) as { default: Array<T> };
+    TestData = data_module.default;
+  } catch (err) {
+    TestData = [];
+  }
   return TestData;
-};
+}
 
-export async function fetchPersonData(fileName: string) {
-  const data_module = await import(`../test-data/${fileName}`, {with: {type: "json"}}) as {default: Array<TestDataObjectPerson>};
-  const TestData: Array<TestDataObjectPerson> = data_module.default;
-  return TestData;
-};
-
-export async function fetchCommentData(fileName: string) {
-  const data_module = await import(`../test-data/${fileName}`, {with: {type: "json"}}) as {default: Array<TestDataObjectComment>};
-  const TestData: Array<TestDataObjectComment> = data_module.default;
-  return TestData;
-};
 
 export async function getListItems(page: Page, test_id: string, selectors: Array<string>) {
   const listItemsLocator = page.getByTestId(test_id).locator('li');
   const listItems = await listItemsLocator.evaluateAll((listItems, selectors) => {
     return Array.from(listItems).map((item) => {
       const listItem: Record<string, string> = {};
-      
+
       if (selectors.length > 1) {
-        listItem.label = item.querySelector(selectors[0])?.textContent?? "";
-        listItem.text = item.querySelector(selectors[1])?.textContent?? "";
+        listItem.label = item.querySelector(selectors[0])?.textContent ?? "";
+        listItem.text = item.querySelector(selectors[1])?.textContent ?? "";
       } else {
-        listItem.text = item.querySelector(selectors[0])?.textContent?? "";
-        listItem.href = item.querySelector(selectors[0])?.getAttribute("href")?? "";
+        listItem.text = item.querySelector(selectors[0])?.textContent ?? "";
+        listItem.href = item.querySelector(selectors[0])?.getAttribute("href") ?? "";
       }
       return listItem;
     });
