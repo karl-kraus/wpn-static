3b72ab22f4e15d543193d402f0e389357cf7d2aa
Barbara Krautgartner <barbara.krautgartner@oeaw.ac.at>
2024-10-29 09:00:28 +0000

    feat: adjust to existing tests

--- e2e/specs/register_views/register_list_view_quote.spec.ts
+++ e2e/specs/register_views/register_list_view_quote.spec.ts
@@ -1,45 +1,20 @@
-import { createReadStream } from "node:fs";
-import { createInterface } from "node:readline";
-
 import { expect, test } from "@playwright/test";
 
-const letter = "A";
-const rendered_content: Array<string> = [];
+import type { TestDataObjectIntertextRegister } from "../../src/interfaces.js";
+import { fetchTestData } from "../../utils/helpers.js";
 
-test("register entries match", async ({ page }) => {
-	// Option 1: compare to current production instance
-	/*
-	await page.goto(
-		`https://kraus1933.ace.oeaw.ac.at/Gesamt.xml?template=register_intertexte.html&letter=${letter}`,
-	);
-	await expect(page.locator(".regentry").first()).toBeVisible();
 
-	for (const regEntry of await page.locator(".regentry").all()) {
-		const text = await regEntry.innerText();
-		rendered_content.push(text);
-	}*/
 
-	// Option 2: compare to scrapped data from current production instance
-	await page.goto("http://localhost:8000/html/register_intertexte.html");
-	await expect(page.locator(`#${letter}`).first()).toBeVisible();
-	const entries = await page.locator(`#${letter} > div`).all();
-	for (const regEntry of entries) {
-		const text = await regEntry.innerText();
-		rendered_content.push(text);
-	}
 
-	//*https://nodejs.org/api/readline.html#example-read-file-stream-line-by-line
-	const fileStream = createReadStream(
-		`tests/data/intertext_list_view/intertext_list_view_${letter}.txt`,
-	);
+for (let i = 65; i <= 90; i++) {
+	const letter = String.fromCharCode(i);
+	const testData: Array<TestDataObjectIntertextRegister> = await fetchTestData(`quote_list_view_${letter}.json`);
+	testData.forEach((testObject: TestDataObjectIntertextRegister) => {
+		test(`register entries match for letter ${letter} ${testObject.sortkey} ${testObject.id}`, async ({ page }) => {
+			await page.goto(`http://localhost:8000/html/register_intertexte.html?letter=${letter}`);
+			await expect(page.locator(`#${letter}-pane`)).toBeVisible();
+			await expect(page.getByTestId(`register_short_info_${letter}_${testObject.sortkey}_${testObject.id}`)).toHaveText(testObject.description);
+		})
+	})
+}
 
-	const rl = createInterface({
-		input: fileStream,
-		crlfDelay: Infinity,
-	});
-	let i = 0;
-	rl.on("line", (line) => {
-		expect(rendered_content[i]).toEqual(line);
-		i++;
-	});
-});
