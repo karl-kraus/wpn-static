9c8b7434c84c1801755706d8ccb9e2eb43df3dbb
linxod <daniel.elsner@oeaw.ac.at>
2025-04-16 10:29:44 +0200

    feat:switched to uv for python build; removed smaller fontsize for inkLongQuote #145

--- shellscripts/export_env_variables.sh
+++ shellscripts/export_env_variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-env_file="secret.env"
+env_file=".secret.env"
 if [ -f $env_file ]; 
 then
-    export $(grep -v '^#' secret.env | xargs)
+    export $(grep -v '^#' .secret.env | xargs)
 else
     echo "GITLAB_SOURCE_TOKEN=''" >> $env_file
     echo "please provide credentials in a totally secure file (${env_file}) (already created)"
