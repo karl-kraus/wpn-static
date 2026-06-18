167b151a4391f9346df02e1b4d8aa914cb0bd4e6
Daniel Elsner <daniel.elsner@oeaw.ac.at>
2026-06-18 14:09:01 +0200

    update screenshots

--- e2e/specs/edition_typo_view.spec.ts
+++ e2e/specs/edition_typo_view.spec.ts
@@ -3,18 +3,18 @@ import { glob } from 'glob';
 import path from 'path';
 
 
-const editionTypoFiles = await glob('html/wit-DfeH*.html');
-const editionTypoFilesT = await glob('html/wit-TFragment2*.html');
-const combined_editionTypoFiles = editionTypoFiles.concat(editionTypoFilesT);
+const editionTypoFiles = await glob('html/wit-*.html');
+// const editionTypoFilesT = await glob('html/wit-TFragment2*.html');
+// const combined_editionTypoFiles = editionTypoFiles.concat(editionTypoFilesT);
 
-const pathsToTest = combined_editionTypoFiles.map(f => path.basename(f))
+const pathsToTest = editionTypoFiles.map(f => path.basename(f))
 
 
 pathsToTest.forEach((path ) => {
   test(`testing edition page for ${path}`, async ({ page }) => {
     test.slow();
    await page.setViewportSize({ width: 1880, height: 1000 });
-    await page.goto(`http://localhost:8000/html/${path}`);
+    await page.goto(`http://localhost:8080/html/${path}`);
     const viewportWidth = page.viewportSize()?.width;
 		const mainElement = page.locator("main");
 		const textElemBox = await mainElement.boundingBox();
