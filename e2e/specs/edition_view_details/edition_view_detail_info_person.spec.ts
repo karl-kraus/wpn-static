de033d742c2ce223f7ca0e21a76f33ab5185ae42
BOberreither <37076522+BOberreither@users.noreply.github.com>
2026-08-10 13:25:23 +0200

    i>o

--- e2e/specs/edition_view_details/edition_view_detail_info_person.spec.ts
+++ e2e/specs/edition_view_details/edition_view_detail_info_person.spec.ts
@@ -8,8 +8,8 @@ import {fetchTestData} from '../../utils/helpers.js';
 
 const pathsToTest: Array<{ label: string; path: string }> = [
 	{
-		label: "[Motti]",
-		path: "motti",
+		label: "[Motto]",
+		path: "motto",
 	},
 ];
 
