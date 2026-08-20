947959f25bd0c2e4573c9b490ac8155e67fca874
BOberreither <37076522+BOberreither@users.noreply.github.com>
2026-08-20 11:05:22 +0200

    screenshot test fix

--- e2e/specs/edition_typo_view.spec.ts
+++ e2e/specs/edition_typo_view.spec.ts
@@ -15,6 +15,7 @@ pathsToTest.forEach((path ) => {
     test.slow();
    await page.setViewportSize({ width: 1880, height: 1000 });
     await page.goto(`http://localhost:8000/html/${path}`);
+    await page.addStyleTag({ content: "#sub_grid_pb, .grid-box-1, .grid-box-2, .grid-box-3, #infocontent-wrapper, #infocontent-hide-btn {height: auto !important; max-height: none !important; overflow: visible !important; min-height: 0 !important;}" });
     const viewportWidth = page.viewportSize()?.width;
 		const mainElement = page.locator("main");
 		const textElemBox = await mainElement.boundingBox();
