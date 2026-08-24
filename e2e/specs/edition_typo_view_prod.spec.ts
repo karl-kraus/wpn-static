46f7b0d181b86e7300ea28d105ce793b64da6e49
Daniel Elsner <daniel.elsner@oeaw.ac.at>
2026-08-24 11:53:48 +0200

    new devpage screenshots; new test against production

---
+++ e2e/specs/edition_typo_view_prod.spec.ts
@@ -0,0 +1,23 @@
+import { expect, test } from "@playwright/test";
+import { glob } from 'glob';
+import path from 'path';
+
+
+const editionTypoFiles = await glob('html/wit-DfeH-*.html');
+
+const pathsToTest = editionTypoFiles.map(f => path.basename(f))
+
+
+pathsToTest.forEach((path ) => {
+  test(`testing edition page for ${path}`, async ({ page }) => {
+    test.slow();
+   await page.setViewportSize({ width: 1880, height: 1000 });
+    await page.goto(`http://localhost:8000/html/${path}`);
+    await page.addStyleTag({ content: "#sub_grid_pb, .grid-box-1, .grid-box-2, .grid-box-3, #infocontent-wrapper, #infocontent-hide-btn {height: auto !important; max-height: none !important; overflow: visible !important; min-height: 0 !important;}" });
+    const viewportWidth = page.viewportSize()?.width;
+		const mainElement = page.locator("main");
+		const textElemBox = await mainElement.boundingBox();
+    await page.addStyleTag({ content: "wpn-header, #facscolumn, #infocolumn, footer {display:none !important;}" });
+    await expect(page).toHaveScreenshot({fullPage: true, timeout: 10000,clip: { x: 0, y: 0, width: Number(viewportWidth), height: Number(textElemBox?.height) } });
+  });
+});
