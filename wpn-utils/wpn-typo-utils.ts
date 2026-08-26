b8a391f7e0964ba9cd53b19acbd3e1572168dbc0
BOberreither <bernhard.oberreither@oeaw.ac.at>
2026-08-26 10:12:00 +0200

    paral facs view fix

--- wpn-utils/wpn-typo-utils.ts
+++ wpn-utils/wpn-typo-utils.ts
@@ -13,7 +13,8 @@ const witnessTypeDimensions: Record<string, { width: number | null; height: numb
 	witnessTypescript2: { width: 18.63, height: 23.4 },
 	witnessNote1: { width: 10.3, height: 18.1 },
 	witnessPrint2: { width: 14.2, height: 21 },
-	witnessTypescript3: { width: 18.63, height: 23.4 }
+	witnessTypescript3: { width: 18.63, height: 23.4 },
+	witnessTypescript4: { width: 20.7, height: 25.9 }
 };
 // Toggle visibility of pagination and info content on dropdown button click
 const paginationButton = document.querySelector('#dropdownMenuButton1') as HTMLElement | null;
